f4c11cade0a906c248fee94ed2078b923fcf5d90
Raphael Stoeckli <github@rabanti.ch>
2026-08-20 08:28:26 +0200

    prepared gh-pages

--- NanoXLSX.Compatibility.Test/SequentialCollection.cs
+++
@@ -1,9 +0,0 @@
-﻿using Xunit;
-
-namespace NanoXLSX.Compatibility.Test
-{
-    [CollectionDefinition(nameof(SequentialCollection), DisableParallelization = true)]
-    public class SequentialCollection
-    {
-    }
-}
